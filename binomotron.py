7ac48a2a3b86aeabf5b6188d6f44c55a84d9dabd
Vincent Boettcher <vincent.boettcher@runbox.no>
2022-10-31 12:05:07 +0100

    Added draft of new binomotron

--- binomotron.py
+++ binomotron.py
@@ -4,7 +4,7 @@ import random
 yabinomotron = mariadb.connect(
   host="localhost",
   user="vincent",
-  password="Solus4.0",
+  password="CuissonLente",
    database="Binomotron"
 )
 
@@ -12,7 +12,7 @@ mycursor = yabinomotron.cursor()
 mycursor.execute("SELECT * FROM Students")
 students_list = mycursor.fetchall()
 
-# Binomotron to students to groups
+# Binomotron to put students into random groups for each brief
 
 # If the list is empty, don't bother
 if students_list == [] :
@@ -20,19 +20,32 @@ if students_list == [] :
 
 # If not, while it's not empty: 
 else :
+    
+    group_length = 4
+    groups_list = []
+    
     while students_list != []:
         
-        # Assign to groups of 2
-        if len(students_list) % 2 == 0 :
-            groups = random.sample(students_list, k = 2)
-            for g in groups: 
+        # Assign to groups
+        if len(students_list) % group_length == 0 :
+            groups = random.sample(students_list, k = group_length)
+            print(groups)
+            for g in groups:
+                groups_list.append(g) 
                 students_list.remove(g)
-            print(f" Binome : {groups[0][0]}, {groups[1][0]}")
+            #print(f" Binome : {groups[0][0]}, {groups[1][0]}")
             
-        # Assign to a group of 3
+        # Assign the rest to another group
         else :
-            groups = random.sample(students_list, k = 3)
-            for g in groups: 
+            alternative_length = group_length - 1 if group_length > 2 else group_length + 1
+            groups = random.sample(students_list, k = alternative_length)
+            print(groups)
+            for g in groups:
+                groups_list.append(g) 
                 students_list.remove(g)
-            print(f" Trinome : {groups[0][0]}, {groups[1][0]}, {groups[2][0]}")
+            #print(f" Trinome : {groups[0][0]}, {groups[1][0]}, {groups[2][0]}")
+    for group in groups_list:
+        group_name = ' '.join(x[0] for x in group)
+        print(group_name)
+    
     exit()
